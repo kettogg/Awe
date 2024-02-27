044056678a6509d34bf4cbcc0195083712058553
re1san <kathareaniket@gmail.com>
2024-02-27 16:42:37 +0530

    Init

--- awesome/widgets/popups/init.lua
+++
@@ -1,5 +0,0 @@
-return {
-    brightness_popups = require(... .. '.brightness'),
-    volume_popus      = require(... .. '.volume'),
-    scrotter          = require(... .. '.scrotter')
-}
--- awesome/widgets/themer/mods/init.lua
+++
@@ -1,6 +0,0 @@
-return {
-    colors   = require(... .. '.colors'),
-    switcher = require(... .. '.switcher'),
-    header   = require(... .. '.header'),
-    maim     = require(... .. '.maim'),
-}
