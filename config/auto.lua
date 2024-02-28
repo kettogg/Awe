27b6981bfbc330248216c6048590ed93badd5d56
re1san <kathareaniket@gmail.com>
2024-02-28 14:11:44 +0530

    Structure changes

---
+++ config/auto.lua
@@ -0,0 +1,24 @@
+-- Programs to be executed on startup
+local awful       = require('awful')
+-- Spawns a program ONCE, when the X session is started.
+local on_start    = awful.spawn.once
+-- Spawns a program every time Awesome is loaded, so once
+-- on startup and on every reload.
+local on_reload   = awful.spawn
+-- Like `on_reload`, but takes commands with shell operators
+-- such as `echo 'biggus' > dickus`.
+local shell       = awful.spawn.with_shell
+local autorun     = true
+
+local autorunApps = {
+  "xcompmgr &",
+	-- "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1", -- Start as service in Flakes :D
+	"nm-applet",
+}
+
+if autorun then
+	for app = 1, #autorunApps do
+		shell(autorunApps[app])
+	end
+end
+
